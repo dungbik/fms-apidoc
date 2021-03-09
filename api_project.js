29a325d04a76e626809624023e9006c98776b9f8
Yoon JungHwan <jhyoon@hura.co.kr>
2021-03-09 12:41:51 +0900

    settings api update

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-09T01:31:18.687Z",
+    "time": "2021-03-09T03:41:02.493Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
